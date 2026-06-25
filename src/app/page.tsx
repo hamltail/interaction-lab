ad762ec61d13bd9034ce5beb4e2ce31e0d795672
h-waji <goo.hiro.w@gmail.com>
2026-06-25 17:49:23 +0900

    準備中ページを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Interaction Lab</h1>
-      <p>Building...</p>
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <Image
+        className="interactive-image"
+        src="/images/under-construction.png"
+        alt="準備中"
+        width={512}
+        height={512}
+        priority
+      />
     </main>
   );
 }
